1cb8dd973113f6fa3bdcc8a51858146101120b45
mkovelamudi <kovelamudi83@gmail.com>
2022-04-24 12:58:35 -0800

    New changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-export { default as Navbar } from '../components/Navigation/Navbar'; 
-export { default as CurrencyCrypto } from './CurrencyCrypto/CurrencyCrypto'; 
-export { default as DetailsCrypto } from './DetailsCrypto/DetailsCrypto'; 
-export { default as CryptoNews } from './CryptoNews/CryptoNews'; 
-export { default as Homepage } from './Homepage/Homepage'; 
-export { default as Prediction } from './Prediction'; 
+export { default as Navbar } from '../components/Navigation/Navbar';
+export { default as CurrencyCrypto } from './CurrencyCrypto/CurrencyCrypto';
+export { default as DetailsCrypto } from './DetailsCrypto/DetailsCrypto';
+export { default as CryptoNews } from './CryptoNews/CryptoNews';
+export { default as Homepage } from './Homepage/Homepage';
+export { default as Prediction } from './Prediction/Prediction';
 export { default as Wallet } from './Wallet';
 
