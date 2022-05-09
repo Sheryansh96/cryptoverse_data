4fb2a7d2413a04953ec8475264bf1de215ad99fa
Shreyas <shreyanshrocknroll@gmail.com>
2022-05-08 21:55:42 -0700

    Update to the something

--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -4,5 +4,7 @@ const router  = express.Router()
 const transactionController = require('./../controller/TransactionController')
 
 router.post('/update',transactionController.updateTable)
+router.post('/getDetails',transactionController.sendDetails)
+router.post('/delUser', transactionController.deleteUser)
 
 module.exports = router
